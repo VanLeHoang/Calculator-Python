9f2368b8ffc6922f89153186d85d9534074980f9
Valeho <van.lehoang111@gmail.com>
2024-09-29 22:22:12 -0400

    Update main.py
    Add function to get value when button is pressed

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+def handle_button_click(clicked_button_text):
+  current_text = result_var.get()
+  if clicked_button_text == "=":
+    expression = current_text.replace("÷","/").replace("x","*")
+    result = eval(expression)
