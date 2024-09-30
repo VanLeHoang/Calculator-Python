82697a8620fc157d2d0d805f2a974eba887d02a1
Valeho <van.lehoang111@gmail.com>
2024-09-30 08:28:46 -0400

    Update main.py
    continue the main function

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from tkinter import ttk
 def handle_button_click(clicked_button_text):
   current_text = result_var.get()
   if clicked_button_text == "=":
-    expression = current_text.replace("÷","/").replace("x","*")
-    result = eval(expression)
+    try:
+      expression = current_text.replace("÷","/").replace("x","*")
+      result = eval(expression)
+      if result.is_interger():
+        result = int(result)
+        result_var.set(result)
+    except Exception as e:
+      result_var.set("Error")
