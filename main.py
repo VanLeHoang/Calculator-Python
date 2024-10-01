d99550bb060ef81103af11ec3b67776556ce1222
Valeho <van.lehoang111@gmail.com>
2024-10-01 17:55:18 -0400

    Update main.py
    Update main function

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ def handle_button_click(clicked_button_text):
         result_var.set(result)
     except Exception as e:
       result_var.set("Error")
+  elif clicked_button_text == "C":
+    result_var.set("")
+  elif clicked_button_text == "%":
+    try:
+      current_number = float(current_text)
+      result_var.set(current_number / 100)
+    except ValueError:
+      result_var.set("Error")
